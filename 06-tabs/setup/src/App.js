26688a019fd32585c2b3b1043537346d74805eff
olasunkanmi <olasunkanmiibalogun@gmail.com>
2022-01-05 04:55:49 +0100

    completed all but the last tutorial

--- 06-tabs/setup/src/App.js
+++ 06-tabs/setup/src/App.js
@@ -26,7 +26,7 @@ function App() {
       </section>
     )
   }
-  const {company, dates, duties, title} = jobs[value];
+  const {company, dates, duties, title} = jobs[value];//the index of array destructured here depends on the value of the index on the button clicked. On loading the page, it's oing to be 0(the first value)
   return (   
   <section className='section'>
   <div className='title'>
@@ -35,17 +35,18 @@ function App() {
     </div>
     <div className='job-center'>
       {/* btn container */} 
-     {jobs.map((item,index) =>{
+     {jobs.map((item,index) =>{// here you map throught the whole jobs and return a button for a particular key in each obj.
       return (
         <button key={item.id} onClick={() => setValue(index)} className={`job-btn ${ index=== value && 'active-btn'}`}>{item.company}</button>
         )
      })};
+     {/*at the end value is changes to the index of the btn that was clicked */}
           {/*job info */}
      <article className='job-info'>
        <h3>{title}</h3>
        <h4>{company}</h4>
        <p className='job-date'>{dates}</p>
-       {duties.map((duty,index) => {
+       {duties.map((duty,index) => {//the duties of that particular jobs whose value is now the index is destructured here 
          return (
          <div key={index} className='job-desc'>
           <FaAngleDoubleRight className='job-icon'/> 
